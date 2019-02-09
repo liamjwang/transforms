640d4873d0d855bb21e3c64007645cf6d01fa075
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-02-09 10:26:29 -0800

    Add relativeTransform3D field

--- src/main/java/org/liamwang/transforms/CoordinateFrame.java
+++ src/main/java/org/liamwang/transforms/CoordinateFrame.java
@@ -1,10 +1,21 @@
 package org.liamwang.transforms;
 
+import org.liamwang.transforms.threed.Transform3D;
+
 public class CoordinateFrame {
 
     private final String name;
+    private Transform3D relativeTransform3D;
 
     public CoordinateFrame(String name) {
         this.name = name;
+    }
+
+    public void setRelativeTransform(Transform3D relativeTransform3D) {
+        this.relativeTransform3D = relativeTransform3D;
+    }
+
+    public Transform3D getRelativeTransform3D() {
+        return relativeTransform3D;
     }
 }
