eb7a67f5054bb19ba01ad43e8ea254bf07602927
wangl5 <wangl5@users.noreply.github.com>
2019-02-14 18:17:29 -0800

    Add unit tests for transformManager

--- src/test/java/org/liamwang/transforms/TransformManagerTest.java
+++ src/test/java/org/liamwang/transforms/TransformManagerTest.java
@@ -1,27 +1,42 @@
 package org.liamwang.transforms;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.liamwang.transforms.threed.Transform3D;
 
 public class TransformManagerTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void repeatSendTransformTest() {
         TransformManager tf = new TransformManager();
 
-        tf.sendTransform(new Transform3DStamped("base_link2", "wow",
-            new Transform3D(1, 1, 0)));
-
-        tf.sendTransform(new Transform3DStamped("odom", "base_link2",
-            new Transform3D(1, 1, 0)));
-
-        tf.sendTransform(new Transform3DStamped("limelight", "vision_target",
-            new Transform3D(1, 0, 0)));
-
+        Transform3D a = new Transform3D(1, 0, Math.PI / 4);
         tf.sendTransform(new Transform3DStamped("odom", "base_link",
-            new Transform3D(1, 0, 0)));
+            a));
+        Transform3D b = tf.getTransform("odom", "base_link");
+        assertEquals(a, b);
+        tf.sendTransform(new Transform3DStamped("odom", "base_link",
+            a));
+        Transform3D c = tf.getTransform("odom", "base_link");
+        assertEquals(a, c);
+        tf.sendTransform(new Transform3DStamped("odom", "base_link",
+            a));
+        Transform3D d = tf.getTransform("odom", "base_link");
+        assertEquals(a, d);
+    }
 
+    @Test
+    public void rootTransformSendTest() {
+        TransformManager tf = new TransformManager();
+        Transform3D b = new Transform3D(
+            1, 0, 0, 0, 0, 0);
         tf.sendTransform(new Transform3DStamped("base_link", "limelight",
-            new Transform3D(1, 0, 0)));
-
-        System.out.println(tf.getTransform("vision_target", "wow"));
+            b));
+        Transform3D a = new Transform3D(5, 0, 0, 0, 0, 3);
+        tf.sendTransform(new Transform3DStamped("odom", "base_link",
+            a));
+        Transform3D c = tf.getTransform("odom", "limelight");
+        assertEquals(a.add(b), c);
     }
 }
